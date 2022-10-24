1d66bf794510603b9360e0663fb52ffd0e0902e5
burde041 <burde041@morris.umn.edu>
2022-10-24 00:21:56 -0500

    Change variable names in EchoServer.java

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,31 +1,32 @@
 package echoserver;
+
 import java.net.*;
 import java.io.*;
 
 public class EchoServer {
-	public static final int PORT_NUMBER = 6013;
-	public static final int BUFFER_SIZE = 1024;
+  public static final int PORT_NUMBER = 6013;
+  public static final int BUFFER_SIZE = 1024;
 
-	public static void main(String[] args){
-		try{
-			ServerSocket s = new ServerSocket(PORT_NUMBER);
+  public static void main(String[] args) {
+    try {
+      ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 
-			while(true){
-				Socket clientSocket = s.accept();
-				InputStream in = clientSocket.getInputStream();
-				OutputStream out = clientSocket.getOutputStream();
-				byte buffer = new byte[BUFFER_SIZE];
-				int readBytes;
+      while (true) {
+        Socket socketToClient = serverSocket.accept();
+        InputStream in = socketToClient.getInputStream();
+        OutputStream out = socketToClient.getOutputStream();
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead;
 
-				while((readBytes = in.read(buffer)) != -1){
-					out.write(buffer,0,readBytes);
-					out.flush();
-				}
-				clientSocket.shutdownOutput();
-			}
-		} catch (IOException ioe){
-			System.err.println("Unexpected error");
-			ioe.printStackTrace();
-		}
-	}
+        while ((bytesRead = in.read(buffer)) != -1) {
+          out.write(buffer, 0, bytesRead);
+          out.flush();
+        }
+        socketToClient.shutdownOutput();
+      }
+    } catch (IOException ioe) {
+      System.err.println("Unexpected exception:");
+      ioe.printStackTrace();
+    }
+  }
 }
