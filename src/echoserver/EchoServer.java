182e457c93f39d07af90597b1ac396dd76bebc91
burde041 <burde041@morris.umn.edu>
2022-10-23 23:56:46 -0500

    Implement EchoServer.java

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,31 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+	public static final int PORT_NUMBER = 6013;
+	public static final int BUFFER_SIZE = 1024;
+
+	public static void main(String[] args){
+		try{
+			ServerSocket s = new ServerSocket(PORT_NUMBER);
+
+			while(true){
+				Socket clientSocket = s.accept();
+				InputStream input = clientSocket.getInputStream();
+				OutputStream output = clientSocket.getOutputStream();
+				byte buffer = new byte[BUFFER_SIZE];
+				int readBytes;
+
+				while((readBytes = input.read(buffer)) != -1){
+					output.write(buffer,0,readBytes);
+					output.flush();
+				}
+				clientSocket.shutdownOutput();
+			}
+		} catch (IOException ioe){
+			System.err.println("Unexpected error");
+			ioe.printStackTrace();
+		}
+	}
 }
